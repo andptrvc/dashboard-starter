8bd8f7830ea0e442dcf41f4505b5492f31b8fb8e
andptrvc <mpetrovtsiy@gmail.com>
2025-02-28 20:30:08 +0100

    Pre charts work

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,56 @@
+import Image from "next/image";
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <div className="flex items-center justify-between p-4">
+      <div className="hidden md:flex items-center gap-2 text-xs rounded-full ring-[2px] ring-gray-300 px-2">
+        <Image
+          src="/search.png"
+          alt="search"
+          width={12}
+          height={12}
+        />
+        <input
+          type="text"
+          placeholder="Search..."
+          className="w-[200px] p-2 bg-transparent outline-none"
+        />
+      </div>
+      <div className="flex items-center gap-6 justify-end w-full">
+        <div className="rounded-full bg-white w-7 h-7 flex items-center justify-center cursor-pointer">
+          <Image
+            src="/message.png"
+            alt="message"
+            width={20}
+            height={20}
+          />
+        </div>
+        <div className="rounded-full bg-white w-7 h-7 flex items-center justify-center cursor-pointer relative">
+          <Image
+            src="/announcement.png"
+            alt="announcement"
+            width={20}
+            height={20}
+          />
+          <div className="absolute -top-3 -right-3 w-5 h-5 flex items-center justify-center bg-purple-500 text-white rounded-full text-xs">
+            1
+          </div>
+        </div>
+        <div className="flex flex-col">
+          <span className="text-xs leading-3 font-medium">John Doe</span>
+          <span className="text-[10px] text-gray-500 text-right">Admin</span>
+        </div>
+        <Image
+          src="/avatar.png"
+          alt="avatar"
+          className="rounded-full"
+          height={36}
+          width={36}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
