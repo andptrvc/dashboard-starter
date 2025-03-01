28aa6c02f50ff2bf7d01b0448ef17b6174b34efb
andptrvc <mpetrovtsiy@gmail.com>
2025-03-01 23:02:29 +0100

    Added count chart and attendace chart

--- src/app/(dashboard)/admin/page.tsx
+++ src/app/(dashboard)/admin/page.tsx
@@ -1,16 +1,31 @@
+import AttendanceChart from "@/components/AttendanceChart";
+import CountChart from "@/components/CountChart";
 import UserCard from "@/components/UserCard";
 import React from "react";
 
 const AdminPage = () => {
   return (
     <div className="flex p-4 gap-4 flex-col md:flex-row">
-      <div className="w-full lg:w-2/3">
+      <div className="w-full lg:w-2/3 flex flex-col gap-8">
         <div className="flex gap-4 justify-between flex-wrap">
           <UserCard type="student" />
           <UserCard type="teacher" />
           <UserCard type="parent" />
           <UserCard type="staff" />
         </div>
+        {/* middle chart */}
+        <div className="flex gap-4 flex-col lg:flex-row">
+          {/* cchart */}
+          <div className="w-full lg:w-1/3 h-[450px]">
+            <CountChart />
+          </div>
+          {/* achart */}
+          <div className="w-full lg:w-2/3 h-[450px]">
+            <AttendanceChart />2
+          </div>
+        </div>
+        {/* bottom chart */}
+        <div>2</div>
       </div>
       <div className="w-full lg:w-1/3">r</div>
     </div>
