eaa705052501390b8ac204013242d07f8300e1ee
andptrvc <mpetrovtsiy@gmail.com>
2025-03-02 02:12:58 +0100

    Pages done

---
+++ src/app/(dashboard)/parent/page.tsx
@@ -0,0 +1,24 @@
+"use client";
+
+import Announcements from "@/components/Announcements";
+import BigCalendar from "@/components/BigCalendar";
+
+const ParentPage = () => {
+  return (
+    <div className="flex p-4 gap-4 flex-col xl:flex-row flex-1">
+      {/* left */}
+      <div className="w-full xl:w-2/3">
+        <div className="h-full bg-white p-4 rounded-md">
+          <h1 className="text-xl font-semibold">Schedule (John Doe)</h1>
+          <BigCalendar />
+        </div>
+      </div>
+      {/* right */}
+      <div className="w-full xl:w-1/3 flex flex-col gap-8">
+        <Announcements />
+      </div>
+    </div>
+  );
+};
+
+export default ParentPage;
